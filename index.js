e88e3ee115f7a34b019e48e631c58a78fa70698f
anh2 <phuongyon9@gmail.com>
2022-11-24 16:04:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let data = [
+	{ id: 1, content: "CONTENT MENU 1" },
+	{ id: 2, content: "CONTENT MENU 2" },
+	{ id: 3, content: "CONTENT MENU 3" },
+];
+
+const getELE = (id) => {
+	return document.getElementById(id);
+};
+
+const activeMenu = () => {
+	let nav = getELE("menu_nav");
+	let act = nav.getElementsByClassName("menu_btn");
+	for (let i = 0; i < act.length; i++) {
+		act[i].addEventListener("click", function () {
+			let current = document.getElementsByClassName("active");
+			current[0].className = current[0].className.replace(" active", "");
+			this.className += " active";
+		});
+	}
+};
+activeMenu();
+
+const clickMenu = (e) => {
+	let getID = e.target.getAttribute("data-id");
+	let content = getELE("content");
+	data.map((item) => {
+		if (getID == item.id) {
+			content.innerHTML = `<p class="content_text">${item.content}</p>`;
+		}
+	});
+};
+
+const closeMenu = () => {
+	let menu = getELE("menu_nav");
+	menu.style.display = "none";
+};
+closeMenu();
+
+const showMenu = () => {
+	let menu = getELE("menu_nav");
+
+	menu.style.display = "block";
+};
